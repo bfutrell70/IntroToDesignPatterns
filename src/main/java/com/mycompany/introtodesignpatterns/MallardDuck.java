e6379280b626845f9e24856b34705d04eed909e3
Brian Futrell <bfutrel@gmail.com>
2019-06-05 12:53:19 -0400

    intial commit

---
+++ src/main/java/com/mycompany/introtodesignpatterns/MallardDuck.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.introtodesignpatterns;
+
+/**
+ *
+ * @author wfutr
+ */
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+    
+    public void display() {
+        System.out.println("I'm a real mallard duck!");
+    }
+}
